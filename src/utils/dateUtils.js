8e86042ca79ac1ee00319d1369b457557c11a1d0
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2025-01-04 09:58:17 +0530

    Create dateUtils.js

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,15 @@
+export const formatDateForInput = (dateString) => {
+  if (!dateString) return '';
+  const date = new Date(dateString);
+  return date.toISOString().split('T')[0];
+};
+
+export const formatDateDisplay = (dateString) => {
+  if (!dateString) return '-';
+  const date = new Date(dateString);
+  return date.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  }).replace(/\//g, '/');
+};
