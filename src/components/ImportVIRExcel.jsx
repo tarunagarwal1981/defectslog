44ccf80310a43caea2deebb525f025ed69ab3093
tarunagarwal1981 <tarunagarwal1981@gmail.com>
2025-04-28 07:12:02 +0530

    1

--- src/components/ImportVIRExcel.jsx
+++ src/components/ImportVIRExcel.jsx
@@ -229,7 +229,7 @@ const ImportVIRDialog = ({ isOpen, onClose, vesselNames, onImportComplete }) => 
               vessel_name: detectedVessel.name,
               'Status (Vessel)': 'OPEN',
               'Date Reported': new Date().toISOString().split('T')[0],
-              external_visibility: false,
+              external_visibility: true,
               initial_files: [],
               completion_files: [],
               'raised_by': 'VIR'
